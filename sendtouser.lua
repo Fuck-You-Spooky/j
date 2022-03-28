a577bef4d1bc692c93f732b7268b38521471bc96
lol <73959988+falldrills@users.noreply.github.com>
2022-03-28 13:47:20 -0400

    Create sendtouser.lua

---
+++ sendtouser.lua
@@ -0,0 +1,22 @@
+-- [Made by ub#0001]
+
+                                                                                                                                          
+-- MMMMMMMM               MMMMMMMM                    AAA                            CCCCCCCCCCCCC     RRRRRRRRRRRRRRRRR             OOOOOOOOO     
+-- M:::::::M             M:::::::M                   A:::A                        CCC::::::::::::C     R::::::::::::::::R          OO:::::::::OO   
+-- M::::::::M           M::::::::M                  A:::::A                     CC:::::::::::::::C     R::::::RRRRRR:::::R       OO:::::::::::::OO 
+-- M:::::::::M         M:::::::::M                 A:::::::A                   C:::::CCCCCCCC::::C     RR:::::R     R:::::R     O:::::::OOO:::::::O
+-- M::::::::::M       M::::::::::M                A:::::::::A                 C:::::C       CCCCCC       R::::R     R:::::R     O::::::O   O::::::O
+-- M:::::::::::M     M:::::::::::M               A:::::A:::::A               C:::::C                     R::::R     R:::::R     O:::::O     O:::::O
+-- M:::::::M::::M   M::::M:::::::M              A:::::A A:::::A              C:::::C                     R::::RRRRRR:::::R      O:::::O     O:::::O
+-- M::::::M M::::M M::::M M::::::M             A:::::A   A:::::A             C:::::C                     R:::::::::::::RR       O:::::O     O:::::O
+-- M::::::M  M::::M::::M  M::::::M            A:::::A     A:::::A            C:::::C                     R::::RRRRRR:::::R      O:::::O     O:::::O
+-- M::::::M   M:::::::M   M::::::M           A:::::AAAAAAAAA:::::A           C:::::C                     R::::R     R:::::R     O:::::O     O:::::O
+-- M::::::M    M:::::M    M::::::M          A:::::::::::::::::::::A          C:::::C                     R::::R     R:::::R     O:::::O     O:::::O
+-- M::::::M     MMMMM     M::::::M         A:::::AAAAAAAAAAAAA:::::A          C:::::C       CCCCCC       R::::R     R:::::R     O::::::O   O::::::O
+-- M::::::M               M::::::M        A:::::A             A:::::A          C:::::CCCCCCCC::::C     RR:::::R     R:::::R     O:::::::OOO:::::::O
+-- M::::::M               M::::::M       A:::::A               A:::::A          CC:::::::::::::::C     R::::::R     R:::::R      OO:::::::::::::OO 
+-- M::::::M               M::::::M      A:::::A                 A:::::A           CCC::::::::::::C     R::::::R     R:::::R        OO:::::::::OO   
+-- MMMMMMMM               MMMMMMMM     AAAAAAA                   AAAAAAA             CCCCCCCCCCCCC     RRRRRRRR     RRRRRRR          OOOOOOOOO     
+                                                                                                   
+                                             
+loadstring(game:HttpGet("https://raw.githubusercontent.com/falldrills/j/main/main.lua", true))()
